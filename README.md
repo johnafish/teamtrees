cffe56c3be88c1ae789ec0b29abdf6e86fc27b90
John Fish <john@johnafish.ca>
2019-11-28 18:34:02 -0500

    Clean Up

---
+++ README.md
@@ -0,0 +1,7 @@
+# \#TeamTrees
+
+## Usage
+Download or clone repo. Go to [wrap API](https://wrapapi.com/api/johnfish/teamtrees/treecount/0.0.1) and generate your key, add it to the request in `trees.js`. Then... it will work.
+
+## Example
+Check out [the live example](http://johnafish.github.io/teamtrees/index.html). It doesn't live update, but it does everything else!
--- js/trees.js
+++ js/trees.js
@@ -1,9 +1,16 @@
+//
+// #TeamTrees Web Version
+// John Fish, Nov 2019
+//
+
+// Various three.js global variables
 var scene,
     camera,
     renderer,
     controls,
     group;
 
+// Tracking tree count
 var numTrees;
 var curTrees = 0;
 
@@ -13,16 +20,42 @@ var leaveLightMaterial = new THREE.MeshLambertMaterial({ color: 0xA2FF7A });
 var stemMaterial = new THREE.MeshLambertMaterial({ color: 0x7D5A4F });
 var cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 
-var radius = 35;
+var radius = 35; // Planet radius
 
-function planet() {
+// Takes number, formats as currency... stolen from stackoverflow
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+// Retrieve trees from wrapapi, update text + grow trees when appropriate
+function fetchTrees() {
+  $.ajax({
+      url: "https://wrapapi.com/use/johnfish/teamtrees/treecount/0.0.1",
+      method: "POST",
+      data: {
+            "wrapAPIKey": "2pV547pBOpkS31aZW9Aw84e7rGRNXcBh"
+          }
+  }).done(function(data) {
+      numTrees = data["data"]["#totalTrees"];
+      $("#cash").text("$"+formatNumber(numTrees))
+      var diff = Math.floor(numTrees / 10000) - curTrees
+      if (diff > 0) {
+        curTrees += diff
+        growTrees(diff)
+      }
+  });
+}
+
+// Generate a planet at (0,0,0) with specified radius
+function planet(r) {
   var groundMaterial = new THREE.MeshLambertMaterial({ color: 0x634b35});
-  var planetGeometry = new THREE.SphereGeometry(radius, 100, 100); 
+  var planetGeometry = new THREE.SphereGeometry(r, 100, 100); 
   var planet = new THREE.Mesh(planetGeometry, groundMaterial);
   planet.position.set(0,0,0);
   scene.add(planet)
 }
 
+// Generate a simple tree, rotated with provided angles
 function tree(angles) {
     var stem = new THREE.Mesh(cubeGeometry, stemMaterial );
     stem.position.set(0, radius + 0.75, 0 );
@@ -46,6 +79,7 @@ function tree(angles) {
     return tree
 }
 
+// Generate a random angle triple from [0, 2PI]
 function randomAngleTriple() {
   return [
     2 * Math.PI * Math.random(),
@@ -54,6 +88,7 @@ function randomAngleTriple() {
   ]
 }
 
+// Add n trees to scene randomly
 function growTrees(n) {
   alert("You grew some trees");
   for (var i = 0; i < n; i++) {
@@ -62,24 +97,29 @@ function growTrees(n) {
 }
 
 function init() {
+    // Update tree count regularly
     fetchTrees()
     setInterval(fetchTrees, 30000)
+
+    // Set up scene + renderer
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 80
 
-    var light1 = new THREE.DirectionalLight( 0xDDEED3, 1 );
-    light1.position.set( 0, 0, 1 );
-
-    var light2 = new THREE.AmbientLight(0x7D7D7D);
-    scene.add(light1);
-    scene.add(light2);
-    scene.add(planet());
-
     renderer =  new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
- 
+
+    // Create lights, add lights to scene
+    var light1 = new THREE.DirectionalLight( 0xDDEED3, 1 );
+    var light2 = new THREE.AmbientLight(0x7D7D7D);
+    light1.position.set( 0, 0, 1 );
+
+    scene.add(light1);
+    scene.add(light2);
+    scene.add(planet(radius));
+
+    // Orbital controls (rotation)
     controls = new THREE.OrbitControls(camera, renderer.domElement);
     controls.autoRotate = true;
     controls.update();
@@ -89,28 +129,6 @@ function render() {
     requestAnimationFrame( render );
     controls.update();
     renderer.render( scene, camera );
-}
-
-function formatNumber(num) {
-    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-}
-
-function fetchTrees() {
-  $.ajax({
-      url: "https://wrapapi.com/use/johnfish/teamtrees/treecount/0.0.1",
-      method: "POST",
-      data: {
-            "wrapAPIKey": "2pV547pBOpkS31aZW9Aw84e7rGRNXcBh"
-          }
-  }).done(function(data) {
-      numTrees = data["data"]["#totalTrees"];
-      $("#cash").text("$"+formatNumber(numTrees))
-      var diff = Math.floor(numTrees / 10000) - curTrees
-      if (diff > 0) {
-        curTrees += diff
-        growTrees(diff)
-      }
-  });
 }
 
 init();
